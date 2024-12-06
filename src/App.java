5f2224be601379c93cd3971788d2b308521b0e34
cymonearl <ceagalzote01323@usep.edu.ph>
2024-12-06 15:46:09 +0800

    removed some lines

--- src/App.java
+++ src/App.java
@@ -3,10 +3,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Group;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
