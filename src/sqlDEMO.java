83cade32eab81cb1d293ab1ec5fe5822a7d1a91a
cymonearl <ceagalzote01323@usep.edu.ph>
2024-12-06 17:31:23 +0800

    sqlDemo added

--- src/sqlDEMO.java
+++ src/sqlDEMO.java
@@ -8,16 +8,19 @@ import java.sql.SQLException;
 
 public class sqlDEMO {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/dbmssample";
+        String url = "jdbc:mysql://localhost:3306/island_hosting_database";
         String user = "root";
         String password = "";
 
         try {
             Connection connect = DriverManager.getConnection(url, user, password);
-            // String sql = "SELECT * FROM celebrity";
-            // PreparedStatement statement = connect.prepareStatement(sql);
-            // ResultSet result = statement.executeQuery();
-            // System.out.println(result);
+            String sql = "show tables";
+            PreparedStatement statement = connect.prepareStatement(sql);
+            ResultSet result = statement.executeQuery(sql);
+
+            while(result.next()) 
+                System.out.println(result.getString(1));
+
         } catch (SQLException e) {
             e.printStackTrace();
         }        
